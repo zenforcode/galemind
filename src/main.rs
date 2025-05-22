1f51a2b92b83ec3f41abdfde4abbd6e1183aa4c5
S-Mann <29407727+S-Mann@users.noreply.github.com>
2025-05-22 17:00:23 +0100

    ✨ Added axum server and server startup to galemind
    Signed-off-by: S-Mann <29407727+S-Mann@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use axum::{Router, routing::get};
 use clap::Command;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let matches = Command::new("galemind")
         .version("0.1")
         .author("Zenforcode Team <team@zenforcode.com>")
@@ -11,10 +13,25 @@ fn main() {
     // Handle the subcommands
     match matches.subcommand() {
         Some(("start", _sub_matches)) => {
-            println!("Server started with success!");
+            init_http_server().await;
         }
         _ => {
             println!("Use --help for usage.");
         }
     }
+}
+
+async fn init_http_server() {
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
