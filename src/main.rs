1fade6cb2d8ce14de6d8748e8f99627d4653f874
S-Mann <29407727+S-Mann@users.noreply.github.com>
2025-05-22 18:20:19 +0100

    🧐 Added route handling through a hashmap
    Signed-off-by: S-Mann <29407727+S-Mann@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use axum::{Router, routing::get};
 use clap::Command;
+
+mod server;
 
 #[tokio::main]
 async fn main() {
@@ -13,25 +14,10 @@ async fn main() {
     // Handle the subcommands
     match matches.subcommand() {
         Some(("start", _sub_matches)) => {
-            init_http_server().await;
+            println!("Server started with success!");
         }
         _ => {
             println!("Use --help for usage.");
         }
     }
-}
-
-async fn init_http_server() {
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
-
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
 }
--- src/server/server.rs
+++ src/server/server.rs
@@ -1,32 +1,40 @@
 use axum::{Router, routing::get};
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr};
 use tower_http::trace::TraceLayer;
 
 pub struct RestServerBuilder {
     addr: SocketAddr,
+    router: Router,
 }
 
 impl RestServerBuilder {
     pub fn new(addr: &str) -> Self {
         let addr = addr.parse().expect("Invalid address");
-        Self { addr }
+        Self {
+            addr,
+            router: Router::new(),
+        }
     }
 
     pub async fn build(self) -> Result<(), hyper::Error> {
-        let app = Self::create_app();
+        let routeTable =
+            HashMap::from([("/", Self::root_handler), ("/health", Self::health_handler)]);
+
+        for route in routeTable.iter() {}
+        self.router
+            .route("/", get(Self::root_handler))
+            .layer(TraceLayer::new_for_http());
 
         axum::Server::bind(&self.addr)
-            .serve(app.into_make_service())
+            .serve(&self.router.into_make_service())
             .await
     }
 
-    fn create_app() -> Router {
-        Router::new()
-            .route("/", get(Self::root_handler))
-            .layer(TraceLayer::new_for_http())
+    async fn root_handler() -> &'static str {
+        "Hello from Axum Builder!"
     }
 
-    async fn root_handler() -> &'static str {
+    async fn health_handler() -> &'static str {
         "Hello from Axum Builder!"
     }
 }
