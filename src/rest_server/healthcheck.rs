bfd3d003a3f5e87303323bcdf965af99a6538571
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2025-05-22 19:57:51 +0000

    Fixes on healthcheck

---
+++ src/rest_server/healthcheck.rs
@@ -0,0 +1,15 @@
+use axum::response::IntoResponse;
+use axum::{Router, routing::get};
+
+
+async fn liveness_handler() -> impl IntoResponse {
+    "OK"
+}
+async fn readiness_handler() -> impl IntoResponse {
+    "OK"
+}
+pub fn new_router() -> Router {
+    Router::new()
+    .route("/live", get(liveness_handler))
+    .route("/ready", get(readiness_handler))
+}
