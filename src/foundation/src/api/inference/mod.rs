49f595f3ff0a03e970f0b890faf9eed5e945c079
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2025-05-23 22:56:02 +0200

    new file:   foundation/Cargo.lock 	new file:   foundation/src/api/inference/mod.rs 	new file:   foundation/src/api/mod.rs 	new file:   foundation/src/api/model/mod.rs 	new file:   foundation/src/api/tensor.rs 	new file:   foundation/src/lib.rs

---
+++ src/foundation/src/api/inference/mod.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+use crate::api::tensor::{self, Data};
+
+pub enum InferParameter {
+    Bool(bool),
+    Int64(i64),
+    Double(f64),
+    String(String),
+}
+
+pub struct InferenceRequestInput {
+    pub name: String,
+    pub shape: tensor::DataShape,
+    pub datatype: String,
+    pub data: Data,
+}
+
+pub struct InferenceRequestOutput {
+    pub name: String,
+    pub parameters: HashMap<String, InferParameter>,
+}
+
+pub enum InferenceResponse {
+    Ok(InferenceOutput),
+    Error(InferenceError),
+}
+
+pub struct InferenceRequest {
+    pub model_name: String,
+    pub model_version: Option<String>,
+    pub id: String,
+    pub parameters: Option<HashMap<String, InferParameter>>,
+    pub outputs: Option<Vec<InferenceOutput>>,
+}
+
+pub struct InferenceOutput {
+    pub name: String,
+    pub shape: tensor::DataShape,
+    pub datatype: tensor::DataType,
+    pub parameters: Option<HashMap<String, InferParameter>>,
+    pub data: Data,
+}
+
+pub struct InferenceError {
+    pub error: String,
+}
+
+pub trait InferenceProcessor {
+    fn process(&self, _request: InferenceRequest) -> InferenceResponse;
+}
---
+++ src/foundation/src/api/model/mod.rs
@@ -0,0 +1,33 @@
+use crate::api::tensor::{DataShape, DataType};
+
+struct ModelMetaDataRequest {
+    name: String,
+    version: Option<String>,
+}
+
+enum ModelMetaDataResponse {
+    Ok(ModelMetaData),
+    Err(ModelMetaDataError),
+}
+
+struct ModelMetaData {
+    name: String,
+    versions: Option<Vec<String>>,
+    platform: String,
+    inputs: Vec<MetadataTensor>,
+    outputs: Vec<MetadataTensor>,
+}
+
+struct MetadataTensor {
+    name: String,
+    datatype: DataType,
+    shape: DataShape,
+}
+
+struct ModelMetaDataError {
+    error: String,
+}
+
+trait ModelMetaDataProcessor {
+    fn get_model_metadata(&self, request: ModelMetaDataRequest) -> ModelMetaDataResponse;
+}
